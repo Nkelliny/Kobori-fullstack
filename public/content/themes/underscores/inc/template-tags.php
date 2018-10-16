ed3a8a18452cef020a77ea8633617e9065c0cf4a
Kobori ubiratan <ubiratan.jp@outlook.com>
2018-10-16 13:38:29 +0900

    commit-02

--- public/content/themes/underscores/inc/template-tags.php
+++ public/content/themes/underscores/inc/template-tags.php
@@ -82,12 +82,12 @@ function _s_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', '_s' ),
-		sprintf( '<a href="%1$s" rel="bookmark">%2$s</a>',
+	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by </span>', '_s' ),
+		sprintf( '<a href="#" rel="bookmark"></a>',
 			esc_url( get_permalink() ),
 			$time_string
 		),
-		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s">%2$s</a></span>',
+		sprintf( '<span class="author vcard"><a class="url fn n" href="#">%</a></span>',
 			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 			esc_html( get_the_author() )
 		)
