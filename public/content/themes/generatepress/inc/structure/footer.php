6b6a1137fe177cda2237f1f696dacec3b9a298b2
Kobori ubiratan <ubiratan.jp@outlook.com>
2018-10-16 14:14:54 +0900

    commit-02

--- public/content/themes/generatepress/inc/structure/footer.php
+++ public/content/themes/generatepress/inc/structure/footer.php
@@ -75,11 +75,11 @@ if ( ! function_exists( 'generate_add_footer_info' ) ) {
 	 * @since 0.1
 	 */
 	function generate_add_footer_info() {
-		$copyright = sprintf( '<span class="copyright">&copy; %1$s %2$s</span> &bull; %4$s <a href="#" itemprop="url">%5$s</a>',
+		$copyright = sprintf( '<span class="copyright">&copy; Desenvolvido por %2$s</span> &bull; %4$s <a href="#" itemprop="url">%5$s</a>',
 			date( 'Y' ),
-			get_bloginfo( 'Desenvolvido por' ),
+			get_bloginfo( 'name' ),
 			esc_url( 'https://generatepress.com' ),
-			_x( '', 'GeneratePress', 'generatepress' ),
+			_x( 'Kobori', 'Ubiratan', 'generatepress' ),
 			__( 'GeneratePress', 'generatepress' )
 		);
 
